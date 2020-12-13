a8efb83aa6001408ec2c0f6bb4979409557d3d3b
Lucas Dutra <ldutra98@gmail.com>
2020-12-13 17:18:52 -0300

    Add project solution

--- aluno.py
+++ aluno.py
@@ -1,5 +1,5 @@
 # Identifique-se preenchendo as strings neste arquivo
-nome = "Nome Completo"
-matricula = "12/3456789"
-email = "alunx@email.com"
-usuario_github = "gituser"
+nome = "Lucas Dutra Ferreira do Nascimento"
+matricula = "17/0050939"
+email = "ldutra98@gmail.com"
+usuario_github = "lucasdutraf"
