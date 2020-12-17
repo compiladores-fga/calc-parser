464dd903c8a068a90ccb0493bc2ef48c11464342
João Luis Baraky <jlbaraky@gmail.com>
2020-12-17 13:52:33 -0300

    Dados do aluno.

--- aluno.py
+++ aluno.py
@@ -1,5 +1,5 @@
 # Identifique-se preenchendo as strings neste arquivo
-nome = "Nome Completo"
-matricula = "12/3456789"
-email = "alunx@email.com"
-usuario_github = "gituser"
+nome = "João Luis Takur Baraky Dias"
+matricula = "18/0033646"
+email = "jlbaraky@gmail.com"
+usuario_github = "baraky"
