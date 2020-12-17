0cc2305f33decf672861bd364445477088798bff
Felipe Borges <bumbleblo2013@gmail.com>
2020-12-17 05:14:19 -0300

    Adding grammar and update aluno.py information
    Signed-off-by: Felipe Borges <bumbleblo2013@gmail.com>

--- aluno.py
+++ aluno.py
@@ -1,5 +1,5 @@
 # Identifique-se preenchendo as strings neste arquivo
-nome = "Nome Completo"
-matricula = "12/3456789"
-email = "alunx@email.com"
-usuario_github = "gituser"
+nome = "Felipe Borges de Souza Chaves"
+matricula = "16/0049733"
+email = "bumbleblo2013@gmail.com"
+usuario_github = "bumbleblo"
