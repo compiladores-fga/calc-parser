47f6a1e1e5d59098e7da6ae4bbc83f7c2b585357
romulosouza <romulovinicius10@gmail.com>
2020-12-16 22:18:41 -0300

    Fix lint problems

--- parser.py
+++ parser.py
@@ -1,10 +1,9 @@
-import re
 import math
-from lark import Lark, InlineTransformer, Token
+from lark import Lark, InlineTransformer
 
 
-# Implemente a gramática aqui! Você pode testar manualmente seu código executando
-# o arquivo calc.py e testá-lo utilizando o pytest.
+# Implemente a gramática aqui! Você pode testar manualmente seu código
+# executando o arquivo calc.py e testá-lo utilizando o pytest.
 grammar = Lark(
     r"""
     start : assign* comp?
@@ -54,7 +53,8 @@ class CalcTransformer(InlineTransformer):
 
     def __init__(self):
         super().__init__()
-        self.variables = {k: v for k, v in vars(math).items() if not k.startswith("_")}
+        self.variables = {k: v for k, v in vars(math).items()
+                          if not k.startswith("_")}
         self.variables.update(max=max, min=min, abs=abs)
         self.env = {}
 
