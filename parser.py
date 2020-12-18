22d2027fc39a5981dd7efda9c60375b8c211fe03
Davi Antônio da Silva Santos <antoniossdavi@gmail.com>
2020-12-18 15:36:30 -0300

    Implementar a gramática requerida
    Adicionar os recursos exigidos em `README.rst`:
    * operações matemáticas básicas considerando-se a precedência usual
    * operação de potenciação, considerando-se a precedência usual
    * entrada de números inteiros e em ponto flutuante com notação
    científica
    * operadores de comparação implementados usando-se os operadores da
    própria linguagem Python (biblioteca `operator`)
    * definição de variáveis
    * avaliação de variáveis
    * uso de funções matemáticas definidas na biblioteca `math` do Python
    * suporte a múltiplos comandos em um único arquivo

--- parser.py
+++ parser.py
@@ -5,18 +5,86 @@ from lark import Lark, InlineTransformer, Token
 
 # Implemente a gramática aqui! Você pode testar manualmente seu código executando
 # o arquivo calc.py e testá-lo utilizando o pytest.
-grammar = Lark(
-    r"""
-start : /\d+/
+grammar = Lark(r"""
+?start : code
+
+?code : statement+ -> multiple
+
+?statement : expr
+           | assignment
+
+?assignment : NAME "=" expr
+
+?expr : comparison
+      | addition
+
+?comparison : addition ">" addition -> gt
+           | addition "<" addition -> lt
+           | addition ">=" addition -> ge
+           | addition "<=" addition -> le
+           | addition "==" addition -> eq
+           | addition "!=" addition -> ne
+
+?addition : addition "+" multiplication -> add
+          | addition "-" multiplication -> sub
+          | multiplication
+
+?multiplication : multiplication "*" power -> mul
+                | multiplication "/" power -> div
+                | power
+
+?power : atom "^" power
+       | atom
+
+?fargs : "(" expr ("," expr)* ")"
+
+?atom : NUMBER -> number
+      | NAME -> var
+      | "(" expr ")"
+      | "-" atom -> neg
+      | NAME fargs -> fcall
+
+NUMBER : /\d+(\.\d+)?([eE][+-]?\d+)?/
+NAME : /[a-zA-Z_][a-zA-Z_0-9]*/
+COMMENT : /#[^\n]*/
+WHITESPACE : /\s+/
+
+%ignore WHITESPACE
+%ignore COMMENT
 """,
     parser="lalr",
 )
 
 
 class CalcTransformer(InlineTransformer):
-    from operator import add, sub, mul, truediv as div  # ... e mais! 
+    from operator import add, sub, mul, truediv as div, lt, gt, le, ge, eq, ne, neg
 
     def __init__(self):
         super().__init__()
         self.variables = {k: v for k, v in vars(math).items() if not k.startswith("_")}
         self.variables.update(max=max, min=min, abs=abs)
+
+    def assignment(self, name, value):
+        self.variables[name] = value
+        return value
+
+    def var(self, name):
+        return self.variables[name]
+
+    def power(self, x, y):
+        return pow(x, y)
+
+    def number(self, x):
+        try:
+            return int(x)
+        except ValueError:
+            return float(x)
+
+    def fargs(self, *args):
+        return args
+
+    def fcall(self, name, args):
+        return self.variables[name](args)
+
+    def multiple(self, *values):
+        return values[-1]
