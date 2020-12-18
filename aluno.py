6a279bea4f976e688b89ba0ecb16dc48a49252ff
Renan Schadt <welzrenan@gmail.com>
2020-12-18 01:32:40 -0400

    Add student data.

--- aluno.py
+++ aluno.py
@@ -1,5 +1,5 @@
 # Identifique-se preenchendo as strings neste arquivo
-nome = "Nome Completo"
-matricula = "12/3456789"
-email = "alunx@email.com"
-usuario_github = "gituser"
+nome = "Renan Welz Schadt"
+matricula = "16/0143403"
+email = "renanschadt@gmail.com"
+usuario_github = "renan601"
