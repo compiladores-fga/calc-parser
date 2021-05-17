489596695955640dbdc6594666c3746fed511312
JaimeJuan11 <jaime_ruan@hotmail.com>
2021-05-17 20:48:01 -0300

    subindo arquivos

--- aluno.py
+++ aluno.py
@@ -1,5 +1,5 @@
 # Identifique-se preenchendo as strings neste arquivo
-nome = "Nome Completo"
-matricula = "12/3456789"
-email = "alunx@email.com"
-usuario_github = "gituser"
+nome = "Jaime Juan de Castro Feliciano Damasceno"
+matricula = "18/0102711"
+email = "jaime_ruan@hotmail.com"
+usuario_github = "JaimeJuan11"
